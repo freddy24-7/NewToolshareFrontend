ba9d0f6bc7af0ac0f39dcd104af32a0114bd1051
freddy24-7 <freddy.austli@aol.com>
2023-02-10 09:55:35 +0100

    test

--- src/components/Register-Login/RegistrationForm.js
+++ src/components/Register-Login/RegistrationForm.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {SIGN_UP_URL} from "../../backend-urls/constants";
+import classes from "./RegistrationForm.module.css";
 
 const Registration = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -20,10 +23,15 @@ const Registration = () => {
             setErrorMessage('Password is required');
             return;
         }
+        if (password.length < 8 || password.length > 16) {
+            setErrorMessage('Password must be between 8 and 16 characters');
+            return;
+        }
         if (password !== repeatPassword) {
             setErrorMessage('Passwords do not match');
             return;
         }
+
 
         // Clear error message
         setErrorMessage('');
@@ -41,6 +49,15 @@ const Registration = () => {
             })
             .catch(error => {
                 console.error(error);
+                //Checking error response stats
+                console.log(error.response.status);
+                //storing it in a variable
+                const errorCheck = (error.response.status)
+                //setting the error
+                if (errorCheck === 409) {
+                    setError("Gebruikersnaam In gebruik, kies een andere gebruikersnaam")
+                }
+                setIsLoading(false);
             });
     };
 
@@ -64,6 +81,8 @@ const Registration = () => {
                 </div>
             )}
             <button type="submit">Submit</button>
+            {isLoading && <p>Your inputs are ok!</p>}
+            {error && <div className={classes.error}> {error} </div>}
         </form>
     );
 };
